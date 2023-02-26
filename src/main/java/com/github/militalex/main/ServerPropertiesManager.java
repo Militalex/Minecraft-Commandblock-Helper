9b7dbdb3f791dc22e83f797a5cd5e891713627d7
Militalex <LeyAlexander5@web.de>
2023-02-26 15:17:03 +0100

    document and small changes

--- src/main/java/com/github/militalex/main/ServerPropertiesManager.java
+++ src/main/java/com/github/militalex/main/ServerPropertiesManager.java
@@ -1,6 +1,6 @@
 package com.github.militalex.main;
 
-import com.github.militalex.util.datapack.DatapackManager;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,10 +9,11 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
 /**
- * This class can read the server.properties.
+ * This class enables reading server.properties. Only one instance can exist of this class because it follows singleton pattern.
  *
  * @author Militalex
  * @version 1.2
@@ -20,20 +21,34 @@ import java.util.logging.Level;
 public class ServerPropertiesManager {
 
     // Singleton Pattern
+    /**
+     * The only one instance.
+     */
     private static ServerPropertiesManager manager;
 
+    /**
+     * @return Returns the only one instance.
+     */
     public static ServerPropertiesManager getInstance() {
         if (manager == null) manager = new ServerPropertiesManager();
         return manager;
     }
     // Singleton Pattern
 
-    private final HashMap<String, String> properties = new HashMap<>();
+    /**
+     * Saves the server.properties as key value pairs.
+     */
+    private final Map<String, String> properties = new HashMap<>();
 
+    /**
+     * Reading the server.properties and construct properties hash map.
+     */
     private ServerPropertiesManager() {
+        // Path to server.properties file
         final Path propertiesFiles = Paths.get("server.properties");
-        final List<String> lines;
 
+        // Read server.properties line by line
+        final List<String> lines;
         try {
             lines = Files.readAllLines(propertiesFiles);
         } catch (IOException e) {
@@ -41,6 +56,7 @@ public class ServerPropertiesManager {
             throw new RuntimeException("Cannot load Server Properties!", e);
         }
 
+        // Construct key value pairs from line by line, by splitting on "=".
         for (int i = 2; i < lines.size(); i++){
             final List<String> keyValue = Arrays.stream(lines.get(i).split("=")).toList();
 
@@ -51,7 +67,10 @@ public class ServerPropertiesManager {
         }
     }
 
-    public String getProperty(String key){
+    /**
+     * @return Returns value from server properties key or null if non-existent.
+     */
+    public @Nullable String getProperty(String key){
         return properties.get(key);
     }
 }
